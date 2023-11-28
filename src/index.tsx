240cc1fea63d4c907a7d42395ad298d84fa9069a
rouanis5 <ouanis42@gmail.com>
2023-11-28 19:50:10 +0100

    calc the sum of two numbers

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import Main from './views/Main'
 const app = new Elysia()
   .use(html())
   .get('/', () => <Main />)
+  .put('/calc', ({ body }) => {
+    return Number.parseInt(body.num0) + Number.parseInt(body.num1)
+  })
   .listen(process.env.PORT || 8080)
 
 console.log(
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -5,9 +5,18 @@ export const Main = ({ body }: { body?: any }) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Uttunen</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+        {/* <script src="https://cdn.tailwindcss.com"></script> */}
       </head>
-      <body>{body}</body>
+      <body>
+        <form hx-put="/calc" hx-target="#result" hx-swap="innerHTML">
+          <input type="number" name="num0" value="0" />
+          <input type="number" name="num1" value="0" />
+          <p>
+            result: <b id="result">0</b>
+          </p>
+          <button>submit</button>
+        </form>
+      </body>
       <script
         src="https://unpkg.com/htmx.org@1.9.9"
         integrity="sha384-QFjmbokDn2DjBjq+fM+8LUIVrAgqcNW2s0PjAxHETgRn9l4fvX31ZxDxvwQnyMOX"
