6d8b61cc03e2f0b277de9b547769a36e8fd051a9
rouanis5 <ouanis42@gmail.com>
2023-12-01 20:11:17 +0100

    better & clean UI components

--- src/views/Calc.tsx
+++ src/views/Calc.tsx
@@ -1,48 +1,78 @@
-export const Calc = ({ current }: { current?: string }) => {
+import { formatOperand } from '..'
+
+interface CalcState {
+  current?: string
+  previous?: string
+  operator?: string
+}
+
+export const Calc = ({
+  current = '',
+  previous = '',
+  operator = '',
+}: CalcState) => {
+  console.log({ previous, operator, current })
   return (
-    <div hx-target="this" hx-swap="outerHTML">
-      <p id="screen">{current ?? '0'}</p>
+    <form hx-target="#CalcScreen" hx-swap="outerHTML">
+      <CalcScreen current={current} previous={previous} operator={operator} />
       <button hx-delete="/">C</button>
       <div>
-        <NumberBtn previous={current} next={7} />
-        <NumberBtn previous={current} next={8} />
-        <NumberBtn previous={current} next={9} />
-        <button>/</button>
+        <NumberBtn digit={7} />
+        <NumberBtn digit={8} />
+        <NumberBtn digit={9} />
+        <OperatorBtn operator="/" />
       </div>
       <div>
-        <NumberBtn previous={current} next={4} />
-        <NumberBtn previous={current} next={5} />
-        <NumberBtn previous={current} next={6} />
-        <button>*</button>
+        <NumberBtn digit={4} />
+        <NumberBtn digit={5} />
+        <NumberBtn digit={6} />
+        <OperatorBtn operator="*" />
       </div>
       <div>
-        <NumberBtn previous={current} next={1} />
-        <NumberBtn previous={current} next={2} />
-        <NumberBtn previous={current} next={3} />
-        <button>-</button>
+        <NumberBtn digit={1} />
+        <NumberBtn digit={2} />
+        <NumberBtn digit={3} />
+        <OperatorBtn operator="-" />
       </div>
       <div>
-        <NumberBtn previous={current} next={0} />
-        <NumberBtn previous={current} next={'.'} />
-        <button>=</button>
-        <button>+</button>
+        <NumberBtn digit={0} />
+        <NumberBtn digit="." />
+        <OperatorBtn operator="=" />
+        <OperatorBtn operator="+" />
       </div>
-    </div>
+    </form>
   )
 }
 
-export const NumberBtn = ({
-  previous,
-  next,
-}: {
-  previous?: string
-  next: number | '.'
+export const NumberBtn = ({ digit }: { digit: number | '.' }) => {
+  return (
+    <button hx-put="/insert" name="next" value={digit.toString()}>
+      {digit}
+    </button>
+  )
+}
+
+export const OperatorBtn = (params: {
+  operator: '+' | '-' | '*' | '/' | '='
 }) => {
   return (
-    <button
-      hx-put={`/insert?${!previous ? '' : `prev=${previous}`}&next=${next}`}
-    >
-      {next}
+    <button hx-put="/calc" name="operation" value={params.operator}>
+      {params.operator}
     </button>
+  )
+}
+
+export const CalcScreen = ({
+  current = '',
+  previous = '',
+  operator = '',
+}: CalcState) => {
+  return (
+    <div id="CalcScreen">
+      <p>{!current ? '0' : formatOperand(current)}</p>
+      <input type="hidden" name="previous" value={previous} />
+      <input type="hidden" name="operator" value={operator} />
+      <input type="hidden" name="current" value={current} />
+    </div>
   )
 }
