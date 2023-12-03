b0cf55bce76cfe16fb828a4f7aae0a22d194cd92
rouanis5 <ouanis42@gmail.com>
2023-12-03 17:05:18 +0100

    update calc screen

--- src/calculator/components/CalcScreen.tsx
+++ src/calculator/components/CalcScreen.tsx
@@ -1,13 +1,9 @@
 import CalcState from '../interfaces/CalcState.interface'
 
-const CalcScreen = ({
-  current = '',
-  previous = '',
-  operator = '',
-}: CalcState) => {
+const CalcScreen = ({ current, previous, operator }: CalcState) => {
   return (
     <div id="CalcScreen">
-      <p>{!current ? '0' : current}</p>
+      <p>{current ?? '0'}</p>
       <input type="hidden" name="previous" value={previous} />
       <input type="hidden" name="operator" value={operator} />
       <input type="hidden" name="current" value={current} />
